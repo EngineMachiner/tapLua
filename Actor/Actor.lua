999b9b4d84bb96388e54740a6c77faaf70e1735a
EngineMachiner <mfpc98@gmail.com>
2025-03-05 21:36:34 -0300

    Update Actor.lua
    + Oops forgot to merge it into the table.
    + Add functions for generic classes using the builder function.

--- Actor/Actor.lua
+++ Actor/Actor.lua
@@ -1,4 +1,7 @@
 
+local astro = Astro.Table
+
+
 local Actor = {}        tapLua.Actor = Actor
 
 
@@ -22,3 +25,28 @@ local function actor( input )
     } .. input
 
 end
+
+local function build( class, input )
+
+    input.Class = class      return actor(input)
+
+end
+
+local function sprite( input ) return build( "Sprite", input ) end
+
+local function quad( input ) return build( "Quad", input ) end
+
+local function actorFrame( input ) return build( "ActorFrame", input ) end
+
+local function text( input ) return build( "BitmapText", input ) end
+
+
+local t = {
+    
+    actor = actor,      sprite = sprite,        quad = quad,
+
+    actorFrame = actorFrame,        text = text
+
+}
+
+astro.merge( tapLua, t )
