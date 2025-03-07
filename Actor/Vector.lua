71e3208051c5403552cf2d465b21082b14519c9a
EngineMachiner <mfpc98@gmail.com>
2025-03-07 02:12:01 -0300

    Update Vector.lua
    + Add getters through a "generic" get function using varargs.
    + The parameter for these functions can be a vector builder / constructor function. If it exists it could be a function or the default builder.
    + Add setters by unpacking vectors.

--- Actor/Vector.lua
+++ Actor/Vector.lua
@@ -1,45 +1,125 @@
 
--- Return variable arguments for these.
+-- Actor functions to return variable arguments or vector objects.
+
+local astro = Astro.Type
+
+local isFunction = astro.isFunction
+
+
+-- Default builder => Simple operations.
+
+local Vector = tapLua.Vector.builder()
+
+local function builder( input )
+
+    if not input then return end
+
+
+    local isDefault = not isFunction(input)
+
+    if isDefault then return Vector end
+
+    
+    return input
+
+end
+
+-- Generic getter...
+
+local function get( input, ... )
+
+    local Vector = builder(input)       local a, b, c = ...
+
+    return Vector and Vector( a, b, c ) or a, b, c
+
+end
 
 -- getrotation() exists haha.
 
-local function GetDest(self)
+local function GetRotationVector( self, input )
 
-    return self:GetDestX(), self:GetDestY(), self:GetDestZ()
-
-end
-
-local function GetSize(self)
-
-    return self:GetWidth(), self:GetHeight()
+    return get( input, self:getrotation() )
 
 end
 
-local function GetZoomedSize(self)
+local function GetSize( self, input )
 
-    return self:GetZoomedWidth(), self:GetZoomedHeight()
-
-end
-
-local function GetPos(self)
-
-    return self:GetX(), self:GetY(), self:GetZ()
+    return get( input, self:GetWidth(), self:GetHeight() )
 
 end
 
-local function GetZoomCoords(self)
+local function GetZoomedSize( self, input )
 
-    return self:GetZoomX(), self:GetZoomY(), self:GetZoomZ()
+    return get( input, self:GetZoomedWidth(), self:GetZoomedHeight() )
 
 end
+
+
+local function GetCroppedZoomedSize( self, input )
+
+    return get( input, self:GetCroppedZoomedWidth(), self:GetCroppedZoomedHeight() )
+
+end
+
+local GetCroppedZoomedSize = not tapLua.isLegacy() and GetCroppedZoomedSize
+
+
+local function GetZoomCoords( self, input )
+
+    return get( input, self:GetZoomX(), self:GetZoomY(), self:GetZoomZ() )
+
+end
+
+local function GetPos( self, input )
+
+    return get( input, self:GetX(), self:GetY(), self:GetZ() )
+
+end
+
+local function GetDest( self, input )
+
+    return get( input, self:GetDestX(), self:GetDestY(), self:GetDestZ() )
+
+end
+
+
+local function setRotation( self, vector )
+    
+    local x, y, z = vector:unpack()         self:rotationx(x):rotationy(y):rotationz(z)
+
+end
+
+local function setSizeVector( self, vector ) self:setsize(  vector:unpack()  ) end
+
+local function setZoomVector( self, vector ) 
+    
+    local x, y, z = vector:unpack()         self:zoomto( x, y ):zoomz(z)
+
+end
+
+local function setPos( self, vector )
+    
+    local x, y, z = vector:unpack()         self:xy( x, y ):z(z)
+
+end
+
 
 local t = {
     
-    GetDest = GetDest,
-    
-    GetSize = GetSize,      GetZoomedSize = GetZoomedSize,
+    GetRotationVector = GetRotationVector,
 
-    GetPos = GetPos,        GetZoomCoords = GetZoomCoords
+    GetSize = GetSize,      GetZoomedSize = GetZoomedSize,
+    
+    GetCroppedZoomedSize = GetCroppedZoomedSize,
+    
+    GetZoomCoords = GetZoomCoords,
+    
+    GetPos = GetPos,      GetDest = GetDest,
+
+
+    setRotation = setRotation,          setSizeVector = setSizeVector,
+
+    setZoomVector = setZoomVector,          setPos = setPos
 
 }
 
