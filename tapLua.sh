021acd45e4be6ad3cb68906324593ab9bb3cb252
EngineMachiner <mfpc98@gmail.com>
2025-07-02 12:15:02 -0500

    update setup bash

--- tapLua.sh
+++ tapLua.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-FALLBACK="Appearance/Themes/_fallback";         SCRIPTS="$FALLBACK/Scripts"
-
-MODULES="Modules";          FILE="\"tapLua/tapLua.lua\""
+FALLBACK="Themes/_fallback";        MODULES="Modules";       SCRIPT="\"tapLua/tapLua.lua\""
 
 
 # Check modules folder and assign code.
 
-if [ -d "$FALLBACK/$MODULES" ]; then
+if [ -d "Appearance" ]; then 
 
-    MODULES="$FALLBACK/$MODULES";           CODE="LoadModule($FILE)"
+    FALLBACK="Appearance/$FALLBACK";        MODULES="$FALLBACK/$MODULES"
+        
+    CODE="LoadModule($SCRIPT)"
 
 else
 
@@ -18,11 +18,14 @@ else
 
     package.path = package.path .. \";./?/init.lua\"
 
-    dofile($MODULES/$FILE)
+    dofile($MODULES/$SCRIPT)
 
 EOF
 
 fi
+
+
+SCRIPTS="$FALLBACK/Scripts";        BGANIMATIONS="$FALLBACK/BGAnimations"
 
 
 # Clone repository.
@@ -41,5 +44,19 @@ cat << EOF > "$SCRIPTS/tapLua.lua"
 $CODE
 EOF
 
-
 echo "Done."
+
+
+# Open editor to add persistent actor.
+
+cat << EOF
+
+Add the renderer in ScreenSystemLayer aux as a persistent actor:
+
+tapLua.Load("Sprite/Renderer")
+
+EOF
+
+read -p "Nano will open. Press any key to continue."
+
+sudo nano "$BGANIMATIONS/ScreenSystemLayer aux.lua"
