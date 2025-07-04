661384ed2d1013c82b339381549b71c6b982de21
EngineMachiner <mfpc98@gmail.com>
2025-07-03 19:44:26 -0500

    fix LoadModule function not containing arguments

--- Legacy.lua
+++ Legacy.lua
@@ -4,6 +4,6 @@ local isLegacy = tapLua.isLegacy()          if not isLegacy then return end
 
 -- Load scripts in legacy.
 
-local function loadModule(path) return loadfile( "Modules/" .. path )() end
+local function loadModule( path, ... ) return loadfile( "Modules/" .. path )(...) end
 
 LoadModule = LoadModule or loadModule
